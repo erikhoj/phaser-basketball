914a1a035c879cb7c76a804cb20af1d572006228
Erik <erikhoj.p@gmail.com>
2018-09-02 15:28:31 +0200

    small chnages

--- src/phaserExtensions/BaseScene.js
+++ src/phaserExtensions/BaseScene.js
@@ -9,7 +9,7 @@ class BaseScene extends Phaser.Scene
 			const width = window.innerWidth;
 			const height = window.innerHeight;
 
-			this.physics.world.setBounds(0, 0, width, height, true, true, true, true);
+			this.physics.world.setBounds(0, 0, width, height);
 			this.cameras.main.setSize(width, height);
 
 			this.sys.game.renderer.resize(width, height);
